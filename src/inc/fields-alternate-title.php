688b8c829622584a98faba761a585c5c56015c3c
Eddy Sims <eddy@Libriels-MacBook-Air.local>
2019-08-27 22:17:58 -0600

    Adds hero images

--- src/inc/fields-alternate-title.php
+++ src/inc/fields-alternate-title.php
@@ -20,7 +20,7 @@ if ( function_exists( 'acf_add_local_field_group' ) ) {
         ),
         array(
           'key' => 'hero_image_key',
-          'name' => 'hero_image_key',
+          'name' => 'hero_image',
           'label' => __( 'Hero Image', 'flyfree' ),
           'type' => 'image',
           'preview_size' => 'medium'
