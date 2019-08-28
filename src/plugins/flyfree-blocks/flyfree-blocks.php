2a85b6d75d1161bdde9f7f342cd8e6dfeb7419d6
Eddy Sims <eddy@Libriels-MacBook-Air.local>
2019-08-27 22:45:13 -0600

    Adds flyfree blocks plugin and sets a whitelist

---
+++ src/plugins/flyfree-blocks/flyfree-blocks.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name: FlyFree Blocks
+ * Plugin URI: https://edeesims.com
+ * Description: A plugin that disables a bunch of core blocks and adds some cool new ones
+ * Version: 0.0.1
+ * Author: Eddy Sims
+ * Author URI: https://edeesims.com
+ * License: GPLv2
+ * Text Domain: flyfree
+ * 
+ * @package FlyFreeBlocks
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+require_once plugin_dir_path( __FILE__ ) . '/block-whitelist.php';
